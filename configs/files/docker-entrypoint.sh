917f2321b52da5c71c8d43a49a19773d9ccd9ca2
m0hashi <dr.rp.vncl@gmail.com>
2020-09-25 12:13:23 +0900

    modify

--- configs/files/docker-entrypoint.sh
+++ configs/files/docker-entrypoint.sh
@@ -1,3 +1,5 @@
 echo $1 | sudo -S /usr/sbin/sshd && \
+. /opt/conda/etc/profile.d/conda.sh && \
+conda activate rapids && \
 jupyter-lab --port 8888 --ip 0.0.0.0 --allow-root --notebook-dir=~/workspace \
 --NotebookApp.token='' --no-browser
